012374af890098a006dc72f33cffd096fcb97618
Prathit Dode <150372020+Prathit6@users.noreply.github.com>
2024-05-18 19:21:00 +0530

    Update TestDatabase06.java

--- src/testdatabase06/TestDatabase06.java
+++ src/testdatabase06/TestDatabase06.java
@@ -14,7 +14,8 @@ public class TestDatabase06 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+System.out.println("Jay Jay Shree Ram");
     }
     
 }
